0889d3f4294078c222697644c825ad3db2c29c47
Scott Davis <scott@thirstyhead.com>
2024-10-15 13:38:20 -0600

    initial commit

---
+++ build/CMakeFiles/myzork.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myzork.dir/main.cpp.o"
+  "CMakeFiles/myzork.dir/main.cpp.o.d"
+  "myzork"
+  "myzork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myzork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
